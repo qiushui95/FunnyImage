040f05525a4d630b6f77c1a6dd996500fc60d7d9
杨朝旭 <974577817@qq.com>
2018-09-11 18:14:28 +0800

    add main

---
+++ app/src/main/java/me/yangcx/funnyimage/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.yangcx.funnyimage.ui.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.yangcx.funnyimage.R
+
+class MainActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
